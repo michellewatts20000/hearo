1a1321b0892fbfd894cc190dddb8e8c786cd8ef5
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-31 15:31:32 +1000

    added login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,41 +1,87 @@
-import React from 'react';
-import { Heading, Flex, FormControl, FormLabel, Button, Input } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Heading, Flex, FormControl, FormLabel, Button, Input, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-const Login = () => {
+import Auth from '../utils/auth';
+
+
+
+const Login = (props) => {
+
+
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
 
     return (
-
 <Flex align="center" justify="space-between" wrap="wrap" w="100%" p={4}>
 
-
-
-<Heading mb={10}>Login</Heading>
+ {data ? (
+              <Text>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </Text>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <Heading mb={10}>Login</Heading>
        <FormControl id="email" isRequired>
   <FormLabel>Email</FormLabel>
-  <Input placeholder="email" />
+  <Input placeholder="email"  name="email" className="form-input" type="email" value={formState.email} onChange={handleChange} />
 </FormControl>
      <FormControl mt={5} id="password" isRequired>
   <FormLabel>Password</FormLabel>
-  <Input placeholder="password" />
+  <Input placeholder="password"  className="form-input"  name="password" type="password" value={formState.password} onChange={handleChange} />
 </FormControl>
-
-
  <Button
               variant={'solid'}
               colorScheme={'teal'}
               size={'md'}
-              mt={5}>
+              mt={5}
+              type="submit">
               Login
             </Button>
+            </form>
+             )}
 
+            {error && (
+              <Text>
+                {error.message}
+              </Text>
+            )}
 </Flex>
-
-
     );
-
 };
-
-
-
 
 export default Login;
